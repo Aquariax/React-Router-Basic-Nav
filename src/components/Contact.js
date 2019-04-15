c93d4f29bfcb6f0f8f26f4d7c16af26e0562d8ec
Aquariax <roberthrrs8@gmail.com>
2019-04-15 19:21:44 -0400

    still working out bugs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Contact = () => <h1>Contact</h1>;
+const Contact = () =>{
+    return (
+        <div>
+            <h1>Contact</h1>;
+        </div>
+    )
+}
 
 export default Contact;
